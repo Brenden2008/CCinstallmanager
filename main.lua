f5f4449d0b4d500dc7009b3370a44e1ab1cb5287
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-11-07 14:21:11 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,5 +2,5 @@ startup = io.open ("startup.lua", "w")
 prog = io.open ("ccim/prog.lua", "w")
 
 io.output(startup)
-io.write("print('[INFO]: FreeInstallManager has been activated for this session.')print('Now launching startup.lua')startup=io.open("startup2.lua","w")load(startup,'startup2.lua',nil,_ENV)")
+io.write("print('[INFO]: FreeInstallManager has been activated for this session.')print('Now launching startup.lua')startup=io.open('startup2.lua','w')local a,b=load(startup,'startup2.lua',nil,_ENV)if not a then _G.printError(b)else local c={...}a(c[1])end")
 io.close(startup)
